a6924c179a8e2b51756a0f99c9880fbcbf7bccbc
ira089 <gvir2015@gmail.com>
2024-11-27 13:25:07 +0200

    SignUpPage style

--- src/pages/SignUpPage/index.tsx
+++ src/pages/SignUpPage/index.tsx
@@ -1,9 +1,10 @@
 // import React from "react";
+import styles from "../SignInPage/SignInPage.module.scss";
 
 export const SignUpPage = () => {
   return (
-    <div>
-      <h3>Welcome Back to Your Motivational Space</h3>
+    <div className={styles.header}>
+      <h1>Welcome Back to Your Motivational Space</h1>
       <p>
         Reconnect with a community that inspires. Share your goals,
         achievements, and keep the momentum going. Let's get back to making
