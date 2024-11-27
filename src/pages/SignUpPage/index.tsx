969d7e0e1d0437e4f60b1bbcbb25a2b71fba8fac
ira089 <gvir2015@gmail.com>
2024-11-27 13:21:10 +0200

    SignUpPage start

---
+++ src/pages/SignUpPage/index.tsx
@@ -0,0 +1,14 @@
+// import React from "react";
+
+export const SignUpPage = () => {
+  return (
+    <div>
+      <h3>Welcome Back to Your Motivational Space</h3>
+      <p>
+        Reconnect with a community that inspires. Share your goals,
+        achievements, and keep the momentum going. Let's get back to making
+        progress together
+      </p>
+    </div>
+  );
+};
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,21 +1,25 @@
-import { Routes } from '@/constants'
-import { AuthLayout } from '@/layouts'
-import { SignInPage } from '@/pages'
-import { createBrowserRouter } from 'react-router-dom'
+import { Routes } from "@/constants";
+import { AuthLayout } from "@/layouts";
+import { SignInPage, SignUpPage } from "@/pages";
+import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
-	{
-		path: Routes.ROOT,
-		element: <></>,
-		index: true,
-	},
-	{
-		element: <AuthLayout />,
-		children: [
-			{
-				element: <SignInPage />,
-				path: Routes.SIGN_IN,
-			},
-		],
-	},
-])
+  {
+    path: Routes.ROOT,
+    element: <></>,
+    index: true,
+  },
+  {
+    element: <AuthLayout />,
+    children: [
+      {
+        element: <SignInPage />,
+        path: Routes.SIGN_IN,
+      },
+      {
+        element: <SignUpPage />,
+        path: Routes.SIGN_UP,
+      },
+    ],
+  },
+]);
