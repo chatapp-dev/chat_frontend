777a7275bf15cb9ee372b24ef9b09e33b7024d9c
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-12-06 13:49:00 +0200

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-  plugins: [react()]
-})
+    plugins: [react()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: "modern-compiler",
+                silenceDeprecations: ["import"],
+            },
+        },
+    },
+});
