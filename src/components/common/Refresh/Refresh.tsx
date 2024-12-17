28694257c583504e5fdb9ae47bc6fb6d8e697599
ira089 <gvir2015@gmail.com>
2024-12-17 23:59:56 +0200

    updStuly_1

--- src/components/common/Refresh/Refresh.tsx
+++ src/components/common/Refresh/Refresh.tsx
@@ -1,13 +1,13 @@
-import logo from "@/assets/images/logo.png";
+// import logo from "@/assets/images/logo.png";
 import { RefreshForma } from "@/components/common/Refresh";
 import styles from "../Login/Login.module.scss";
 
 const Refresh = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.logo}>
+      {/* <div className={styles.logo}>
         <img src={logo} alt="Logo" />
-      </div>
+      </div> */}
       <div className={styles.container}>
         <div className={styles.form}>
           <div className={styles.header}>
--- src/layouts/AuthLayout/index.tsx
+++ src/layouts/AuthLayout/index.tsx
@@ -1,16 +1,20 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet } from "react-router-dom";
+import logo from "@/assets/images/logo.png";
 
-import styles from './AuthLayout.module.scss'
+import styles from "./AuthLayout.module.scss";
 
 export const AuthLayout = () => {
-	//TODO: Check whether user is authenticated or not. If yes, redirect him to main page.
-	return (
-		<div className={styles.wrapper}>
-			<div>
-				<div className={styles.content}>
-					<Outlet />
-				</div>
-			</div>
-		</div>
-	)
-}
+  //TODO: Check whether user is authenticated or not. If yes, redirect him to main page.
+  return (
+    <div className={styles.wrapper}>
+      {/* <div> */}
+      <div className={styles.content}>
+        <div className={styles.logo}>
+          <img src={logo} alt="Logo" />
+        </div>
+        <Outlet />
+        {/* </div> */}
+      </div>
+    </div>
+  );
+};
